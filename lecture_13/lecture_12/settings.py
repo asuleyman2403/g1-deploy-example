cf10ba5e87df31d21f6010731cf89b176b584253
assyl.suleiman <assyl.suleiman@litslink.com>
2023-04-17 10:27:31 +0600

    fix: allowed my host

--- lecture_13/lecture_12/settings.py
+++ lecture_13/lecture_12/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)$o^+@7ikn5-^hty96qian*k)z+v_agose-(hyxu8&$_*st$*3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'asuleiman.pythonanywhere.com/'
+]
 
 
 # Application definition
